3333e1b4aba921c977aee50a1c6b84090fc7be42
MatFX <mg_73@gmx.de>
2018-04-10 15:08:57 +0200

    /* selection color */

--- src/control/rgbw/RGBWDimmerControl.java
+++ src/control/rgbw/RGBWDimmerControl.java
@@ -1,10 +1,24 @@
 package control.rgbw;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+import control.universaldisplay.SensorValue;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
@@ -14,7 +28,9 @@ import javafx.scene.paint.Stop;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.scene.transform.Affine;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 
 /**
  * 128x128 Orginalabmasse
@@ -74,9 +90,37 @@ public class RGBWDimmerControl extends Region
 	
 	private Circle anzeigeGlanz;
 	
+	/**
+	 * current rgb color at the middle round view
+	 */
 	private Color currentColorRGB = Color.web("#1A1A96");
 	
+	private Canvas textCanvas;
 	
+	private Text textFirstValue, textFirstMeasuringUnit;
+	
+	private DoubleProperty scaleableFontSize = null;
+	
+	private Font fontVorgabe = null;
+	
+	/**
+	 * white colored light
+	 */
+	private SensorValue majorValue;
+	
+	private static final double GAP_PERCENT = 0.1;
+	
+	private boolean isAnimation = false;
+	
+	private Thread animThread = null;
+	
+	private int startIndex = 0;
+	
+	/**
+	 * compare object
+	 */
+	private Color EMPTY_COLOR = Color.web("#00000000");
+
 	public RGBWDimmerControl()
 	{
 		this.initGraphics();
@@ -84,6 +128,48 @@ public class RGBWDimmerControl extends Region
 	}
 
 	private void registerListener() {
+		
+		
+		canvasColoredCircle.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent event)
+			{
+				PickResult result = event.getPickResult();
+			
+				SnapshotParameters params = new SnapshotParameters();
+				params.setFill(Color.TRANSPARENT);
+				Image snapshot = canvasColoredCircle.snapshot(params, null);
+				
+				
+				
+				/* TODO raus
+				File outputFile = new File("./test.png");
+			    BufferedImage bImage = SwingFXUtils.fromFXImage(snapshot, null);
+			    //try {
+			      try {
+					ImageIO.write(bImage, "png", outputFile);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}*/
+				
+				
+				PixelReader pixReader = snapshot.getPixelReader();
+				
+				Color selectedColor = pixReader.getColor((int)result.getIntersectedPoint().getX(), (int)result.getIntersectedPoint().getY());
+				
+				System.out.println("value " + selectedColor.toString());
+				//"empty" pixel found?
+				if(!selectedColor.toString().equals(EMPTY_COLOR.toString()))
+				{
+					currentColorRGB = selectedColor;
+					anzeigeRGB.setFill(currentColorRGB);
+				}
+			}
+			
+		});
+		
 		widthProperty().addListener(observable -> resize());
 		heightProperty().addListener(observable -> resize());
 		
@@ -127,6 +213,7 @@ public class RGBWDimmerControl extends Region
 		wahlkreis = new Circle();
 		
 		canvasColoredCircle = new Canvas();
+	//	canvasColoredCircle.setMouseTransparent(true);
 		
 		stopArray = new Stop[]{
 				new Stop(0.7414634, Color.web("#00000000")),
@@ -137,6 +224,7 @@ public class RGBWDimmerControl extends Region
 		stopMap.put(StopIndizes.INLAY, stopArray);
 		
 		begrenzerInlay = new Circle();
+		begrenzerInlay.setMouseTransparent(true);
 		
 		deckflaechenBegrenzung = new Circle();
 		deckflaechenBegrenzung.setFill(Color.web("#282828"));
@@ -150,7 +238,7 @@ public class RGBWDimmerControl extends Region
 		
 		deckflaechenBegrenzungOverlay = new Circle();
 				
-		this.getChildren().addAll(hintergrund, rahmen_grundfarbe, rahmen_glanz, basisFarbe, canvasColoredCircle, begrenzerInlay,
+		this.getChildren().addAll(hintergrund, rahmen_grundfarbe, rahmen_glanz, basisFarbe, wahlkreis, canvasColoredCircle, begrenzerInlay,
 				deckflaechenBegrenzung, deckflaechenBegrenzungOverlay);
 		
 		drawMinorTick(width, true);
@@ -191,8 +279,14 @@ public class RGBWDimmerControl extends Region
 		anzeigeGlanz = new Circle();
 		anzeigeGlanz.setOpacity(0.38);
 		
+		textCanvas = new Canvas();
+		textFirstValue = new Text();
+		textFirstValue.setText("");
+		textFirstMeasuringUnit = new Text();
+		textFirstMeasuringUnit.setText("");
 		
-		this.getChildren().addAll(basisAnzeige, anzeigeGlanzRahmen, anzeigeRGB, anzeigeGlanz);
+		
+		this.getChildren().addAll(basisAnzeige, anzeigeGlanzRahmen, anzeigeRGB, textCanvas, anzeigeGlanz);
 	}
 	
 
@@ -255,6 +349,8 @@ public class RGBWDimmerControl extends Region
 		canvasColoredCircle.setHeight(radius*0.9375*2);
 		canvasColoredCircle.setLayoutX(centerX - (radius*0.9375));
 		canvasColoredCircle.setLayoutY(centerY- (radius*0.9375));
+		
+		
 		
 		drawColoredCircle();
 		
@@ -332,6 +428,219 @@ public class RGBWDimmerControl extends Region
 		anzeigeGlanz.setRadius(radius * 0.46875);
 		anzeigeGlanz.setFill(innerShinyGradient);
 		
+		//45 = 100/128 * 45 = 0.3515625
+		double w = size * 0.3515625;
+		double h = size * 0.1;
+		double x = centerX - (w/2); 
+		double y = centerY - (h/2);
+		textCanvas.setWidth(w);
+		textCanvas.setHeight(h);
+		textCanvas.relocate(x, y);
+		
+
+		this.drawTextValues(true);
+		
+	}
+	
+	private void drawTextValues(boolean clearing) 
+	{
+		//Size wird für die Ausrichtung benötigt
+		double gaugeSize  = getWidth() < getHeight() ? getWidth() : getHeight();
+		double w = textCanvas.getWidth();
+		double h = textCanvas.getHeight();
+		
+		GraphicsContext gc = textCanvas.getGraphicsContext2D();
+		
+
+		
+		
+		if(scaleableFontSize == null)
+		{
+			scaleableFontSize = new SimpleDoubleProperty(gaugeSize * 0.12);
+		}
+		else
+			scaleableFontSize.set(gaugeSize * 0.12);
+		
+		if(fontVorgabe == null)
+		{
+			fontVorgabe = new Font("Verdana", 12);
+		}
+		
+		
+		
+		Font font = Font.font(fontVorgabe.getName(), scaleableFontSize.get());
+		
+		//Dieses ist dann zu vollziehen, wenn nur der Wert sich geändert hat.
+		if(clearing)
+		{
+		
+			gc.clearRect(0, 0, w, h);
+		}
+		
+		gc.setFill(Color.web("#FFFFFF"));
+		
+		
+		//Fontsize muss ermittelt werden anhand des größten values
+		if(majorValue != null)
+		{
+			//initial
+			 //Ermittlung nach dem maximal möglichen Zustand
+			 Bounds maxTextAbmasse = this.getMaxTextWidth(font, this.majorValue);
+			 if(maxTextAbmasse.getWidth() < w  && maxTextAbmasse.getHeight() < h)
+			 {
+				 double tempSize = getGreaterFont(gaugeSize * 0.12, w, h, majorValue);
+				 if(tempSize != getFontSize().get())
+						getFontSize().set(tempSize);
+				 
+			 }
+			 else
+			 {
+				 double tempSize = getLesserFont(getFontSize().get(), w, h, majorValue);
+				 if(tempSize != getFontSize().get())
+						getFontSize().set(tempSize);
+			 }
+			 font = Font.font(fontVorgabe.getName(), getFontSize().get());
+		}
+		gc.setFont(font);
+		
+		if(majorValue == null)
+			textFirstMeasuringUnit.setText("");
+		else
+			textFirstMeasuringUnit.setText(" "+majorValue.getMeasurementUnit());
+		textFirstMeasuringUnit.setFont(font);
+		
+		
+		double masseinheitX = w - (textFirstMeasuringUnit.getLayoutBounds().getWidth());// + (gaugeSize * 0.015635));
+	
+		double haelfte =  textFirstMeasuringUnit.getLayoutBounds().getHeight() / 2d;
+		double masseinheitY = h/2d +  (haelfte/2d);
+		
+		
+		gc.setFill(Color.web("#282828"));
+		gc.fillText(textFirstMeasuringUnit.getText(), masseinheitX+2, masseinheitY+2);
+		
+		gc.setFill(Color.WHITE);
+		gc.fillText(textFirstMeasuringUnit.getText(), masseinheitX, masseinheitY);
+		
+		
+			
+		if(majorValue == null)
+			textFirstValue.setText("");
+		else
+			textFirstValue.setText(String.format("%.1f", majorValue.getCurrentValue()));
+		
+		textFirstValue.setFont(font);
+		
+		double valueX = masseinheitX - ((textFirstValue.getLayoutBounds().getWidth())  + (gaugeSize * 0.005635));
+		double valueY = masseinheitY;
+		
+		gc.setFont(font);
+		
+		gc.setFill(Color.web("#282828"));
+		gc.fillText(textFirstValue.getText(), valueX+2, valueY+2);
+	
+		gc.setFill(Color.WHITE);
+		gc.fillText(textFirstValue.getText(), valueX, valueY);
+	}
+	
+
+	public double getGAPPercent()
+	{
+		return GAP_PERCENT;
+	}
+	
+
+	protected double getGreaterFont(double fontSize, double w, double h, SensorValue sensorValue)
+	{	
+		double gapBreite = w * getGAPPercent() * 2;
+		double gapHoehe = h * getGAPPercent() * 2;
+		
+		fontSize = fontSize + 1;
+		Bounds futureBounds = textWidth(fontSize, sensorValue);
+		if((futureBounds.getHeight() + gapHoehe) < h && (futureBounds.getWidth() + gapBreite) < w)
+		{
+			return getGreaterFont(fontSize, w, h, sensorValue);
+		}
+		//eines wieder zurück weil die Abfrage nicht gegriffen hat
+		return fontSize-1;
+	}
+	
+
+	public DoubleProperty getFontSize()
+	{
+		return scaleableFontSize;
+	}
+	
+
+	protected double getLesserFont(double fontSize, double w, double h, SensorValue sensorValue)
+	{	
+		Bounds futureBounds = textWidth(fontSize, sensorValue);
+		double gapBreite = w * getGAPPercent() * 2;
+		double gapHoehe = h * getGAPPercent() * 2;
+		//wenn eines von beiden über das Ziel hinaus ist, so ist eine kleiner Fontgröße zu ermitteln
+		if((futureBounds.getHeight() + (gapHoehe)) > h || (futureBounds.getWidth() + (gapBreite)) > w)
+		{
+			fontSize = fontSize - 1;
+			if(fontSize <= 0)
+				return 1;
+			return getLesserFont(fontSize, w, h, sensorValue);
+		}
+		return fontSize;
+	}
+	
+
+	private Bounds textWidth(double size, SensorValue sensorValue)
+	{
+		//hier muss die bounds aufgebaut werden anhand der zwei darzustellenden Werte 
+		
+		double valueRounded = Math.round(sensorValue.getCurrentValue()*10)/10;
+		String eineNachkomma = String.format("%.1f", valueRounded);
+		String showValue = eineNachkomma + " " + sensorValue.getMeasurementUnit();
+		
+		if(fontVorgabe == null)
+			fontVorgabe = new Font("Verdana", 12);
+		Text text = new Text(showValue);
+		Font font =  Font.font(fontVorgabe.getFamily(), size);
+        text.setFont(font);
+        return text.getBoundsInLocal();
+	}
+	
+	private Bounds getMaxTextWidth(Font font, SensorValue valueSensor) 
+	{
+		String minValue = String.format("%.1f", valueSensor.getVon());
+		String maxValue = String.format("%.1f", valueSensor.getBis());
+		
+		String measuringUnit = " " + valueSensor.getMeasurementUnit();
+		
+		Text valTextMin = new Text(minValue);
+		valTextMin.setFont(font);
+		
+		Bounds valMinBounds = valTextMin.getBoundsInLocal();
+		
+		Text valTextMax = new Text(maxValue);
+		valTextMax.setFont(font);
+		
+		Bounds valMaxBounds = valTextMax.getBoundsInLocal();
+		
+		Text messText = new Text(measuringUnit);
+		messText.setFont(font);
+		
+		Bounds einheitBounds = messText.getBoundsInLocal();
+		
+		double width = valMinBounds.getWidth();
+		if(width < valMaxBounds.getWidth())
+			width = valMaxBounds.getWidth();
+		
+		double height = valMinBounds.getHeight();
+		if(height < valMaxBounds.getHeight())
+			height = valMaxBounds.getHeight();
+		
+		if(height < einheitBounds.getHeight())
+			height = einheitBounds.getHeight();
+		
+		width = width + einheitBounds.getWidth();
+		
+		return new BoundingBox(0,0, width, height);
 	}
 	
 	/**
@@ -410,6 +719,7 @@ public class RGBWDimmerControl extends Region
 		
 		java.awt.Color farbe = new java.awt.Color(0, 0, 0, 0);
 		
+		
 		for (int i = 0; i< 360; i++)
 		{
 			color = Color.hsb(i, 1f, 1f);
@@ -422,6 +732,157 @@ public class RGBWDimmerControl extends Region
 		
 		
 		
+	}
+
+	public void setMajorValue(SensorValue majorValue)
+	{
+		this.majorValue = majorValue;
+		
+	}
+	public void startAnimation() 
+	{
+		if(animThread != null && animThread.isAlive())
+			animThread.stop();
+		
+		isAnimation = true;
+		int minValue = (int)majorValue.getVon();
+		int maxValue = (int)majorValue.getBis();
+		Runnable runnable = new Runnable(){
+
+			@Override
+			public void run() 
+			{
+				Random ran = new Random();
+			
+				while(isAnimation)
+				{
+					int neuerWert = ran.nextInt((maxValue - minValue) + 1);
+					
+					if(neuerWert != majorValue.getCurrentValue())
+					{
+						setCurrentValue(neuerWert, false);
+					}
+					
+					try 
+					{
+						TimeUnit.SECONDS.sleep(2);
+					} 
+					catch (InterruptedException e) 
+					{
+						e.printStackTrace();
+					}
+					
+				}
+				
+				
+			}
+
+		};
+		animThread = new Thread(runnable);
+		animThread.start();
+		
+	}
+
+	public void stopAnimation()
+	{
+		isAnimation = false;
+		if(animThread != null)
+			animThread.stop();
+		
+	}
+	
+	/**
+	 * Methode für das Sezten des aktuellen Zustands
+	 * @param neuerWert
+	 */
+	public void setCurrentValue(double newPercentValue, boolean isInit) 
+	{
+		//wir haben 31 Ticks für die Beleuchtung
+		
+		//ermitteln wieviele ticks wir benötigen
+		int showTickNumber =  (int) Math.round((double)37D/100d * (double)newPercentValue);
+		int showTickNumberAktuell = (int) Math.round((double)37D/100d * (double)majorValue.getCurrentValue());
+	
+		double wertVonEinemTickInProzent = 100/37D;
+		
+		if(!isInit)
+		{
+			if(showTickNumber > showTickNumberAktuell)
+			{
+				
+				startIndex = showTickNumberAktuell + 1 ;
+				do
+				{
+					
+					tickMap.get(startIndex).setStroke(Color.web("#CCC200"));
+					//jeder tick spiegelt einen Wert wieder der in auch zur Anzeige kommen muss
+					
+					double tempValue = wertVonEinemTickInProzent * (double)startIndex;
+					///this.currentValue = (int)Math.round(tempValue);
+					majorValue.setCurrentValue(tempValue);
+					this.drawTextValues(true);
+					
+					//TODO überlegen ob man ständig mitaktualisiert oder erst zum schluss
+					//this.setRotation(currentValue);
+					
+					
+					
+					try 
+					{
+						TimeUnit.MILLISECONDS.sleep(25);
+					} 
+					catch (InterruptedException e) 
+					{
+					
+						e.printStackTrace();
+					}
+					startIndex ++;
+				}
+				while(startIndex <= showTickNumber);
+			}
+			else if(showTickNumber < showTickNumberAktuell)
+			{
+				//hier werden die Ticks abgedunkelt, deswegen beginnen mir mit den vorhergehenden Wert
+				startIndex = showTickNumberAktuell;
+				
+				do
+				{
+					tickMap.get(startIndex).setStroke(Color.web("#626262"));
+					
+					double tempValue = wertVonEinemTickInProzent * (double)startIndex;
+					//this.currentValue = (int)Math.round(tempValue);
+					majorValue.setCurrentValue(tempValue);
+					this.drawTextValues(true);
+					
+				//	this.setRotation(currentValue);
+					try 
+					{
+						TimeUnit.MILLISECONDS.sleep(25);
+					} 
+					catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					startIndex--;
+					
+				}
+				while(startIndex > showTickNumber);
+				
+				
+			}
+			//isAnimation =false;
+
+			//bei gleich wird nichts gemacht
+		}
+	
+		//Übernahme des aktuellen Wertes
+		majorValue.setCurrentValue(newPercentValue);
+		//this.setRotation(majorValue.getCurrentValue());
+		this.drawTextValues(true);
+		
+		
+		
+	
+	
 	}
 
 
--- src/control/rgbw/SingleRGBWControl.java
+++ src/control/rgbw/SingleRGBWControl.java
@@ -1,9 +1,14 @@
 package control.rgbw;
 
+import control.universaldisplay.SensorValue;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -19,9 +24,48 @@ public class SingleRGBWControl extends Application
 	{
 		 BorderPane pane = new BorderPane();
 		 
+		 SensorValue majorValue = new SensorValue(0D, 0D , 100D, "%", "");
+		 
+		 
 		 RGBWDimmerControl rgbwDimmerControl = new RGBWDimmerControl();
+		 rgbwDimmerControl.setMajorValue(majorValue);
 		 
 		 pane.setCenter(rgbwDimmerControl);
+		 
+		 
+		    
+		 VBox vBox = new VBox(2);
+	     vBox.setPadding(new Insets(5, 5, 5, 5));
+	        
+	        //start and stop animation with random values
+	        //it simulate incoming signals from the actuator
+		 ToggleButton test = new ToggleButton("Start");
+		 test.setOnAction(new EventHandler<ActionEvent>(){
+		
+			@Override
+			public void handle(ActionEvent event) 
+			{
+				if(test.isSelected())
+				{
+					
+					rgbwDimmerControl.startAnimation();
+					test.setText("Ende");
+				}
+				else
+				{
+					rgbwDimmerControl.stopAnimation();
+					test.setText("Start");
+				}
+			
+			}
+	    	
+		 });
+		 vBox.getChildren().addAll(test);
+		 
+		 pane.setRight(vBox);
+		 
+		 
+		 
 		 
 		 
 		 Scene scene = new Scene(pane);
